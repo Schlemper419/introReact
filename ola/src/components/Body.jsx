331aba30cea0749596c2215a4f553b38a2f2b8c1
Schlemper419 <gabrielschlemperdaniel@gmail.com>
2025-05-26 22:19:50 -0300

    Introdução ao React

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+
+        <h2 className='titulo2'>Dinos são legais</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, voluptatem ullam qui in blanditiis quo fuga perferendis officiis maiores ipsum quis asperiores aliquid, dolorem aperiam vero ducimus? Dolor, hic animi?</p>
+
+    </div>
+  )
+}
+
+export default Body
