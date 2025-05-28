d71b378ec0cd2e04d2ecfcba2115244dd028d56c
Schlemper419 <gabrielschlemperdaniel@gmail.com>
2025-05-27 22:14:41 -0300

    primeiro form react

--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,13 +1,91 @@
-import React from 'react'
+
+import { useState } from 'react'
 import './Body.css'
 
 function Body() {
+
+  const [cont, setCont] = useState(0)
+  const [inputValor , setInputValor] = useState('')
+  const[inputN1, setInputN1] = useState('')
+  const[inputN2, setInputN2] = useState('')
+
+  function aumentar() {
+    setCont(cont + 1)
+  }
+
+  function diminuir() {
+    if (cont > 0) {
+      setCont(cont - 1)
+    }
+  }
+
+  function alterar(){
+    setCont(Number(inputValor))
+  }
+
+  function somar(){
+    setCont (Number(inputN1) + Number(inputN2))
+  }
+
+  function subtrair(){
+    setCont (Number(inputN1) - Number(inputN2))
+  }
+
+  function multiplicar(){
+    setCont (Number(inputN1) * Number(inputN2))
+  }
+  
+
+
   return (
     <div className='Body'>
 
-        <h2 className='titulo2'>Dinos são legais</h2>
+      <h2 className='titulo2'>Dinos são legais</h2>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, voluptatem ullam qui in blanditiis quo fuga perferendis officiis maiores ipsum quis asperiores aliquid, dolorem aperiam vero ducimus? Dolor, hic animi?</p>
+      <div className='cont'>
+
+        <button onClick={diminuir}>-</button>
+
+        <p>{cont}</p>
+
+        <button onClick={aumentar}>+</button>
+
+
+      </div>
+
+      
+      <div className="form">
+
+         <input type="number"
+         value={inputValor} 
+         onChange={(e)=> setInputValor(e.target.value)} />
+      
+         <button onClick={alterar}>Alterar</button>
+
+      </div>
+
+      <div className="calculadera">
+
+        <input type="text"
+          value={inputN1}
+          onChange={(e) => setInputN1(e.target.value)} />
+        
+
+        <button onClick={somar}>+</button>
+
+        <button onClick={subtrair}>-</button>
+
+        <button onClick={multiplicar}>*</button>
+
+        <input type="text"
+          value={inputN2}
+          onChange={(e) => setInputN2(e.target.value)}  />
+          {inputN2}
+
+
+
+      </div>
+
 
     </div>
   )
